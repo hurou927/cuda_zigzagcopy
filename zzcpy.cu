cc7e264eae2af5c661378d966cb578a4c3c9573e
hurou-cs <god.be.with.ye.fs@gmail.com>
2018-02-07 15:14:48 +0900

    fix:kerenel->kernel

--- zzcpy.cu
+++ zzcpy.cu
@@ -46,7 +46,7 @@ __global__ void zigzagCopy(T* G_IN, T *G_OUT, uint64 *XYPOS, unsigned int INPUTW
 }
 
 template <typename T ,unsigned int TILEWIDTH  ,unsigned int NUMTHREADS>
-void execGPUkerenel(size_t input_width){
+void execGPUkernel(size_t input_width){
 	cudaProfilerStart();
 	cudatimeStamp cudatimer(10);
 	//printf("%d\n",CUDART_VERSION);
@@ -123,12 +123,12 @@ int main(int argc,char **argv){
 	GPUBoost(64);
 
 	for(size_t n=256;n<32*1024;n=n*2)
-		execGPUkerenel<float,  32, 512>(n); // type:float, tilesize:32x32, the numeber of threads:512
+		execGPUkernel<float,  32, 512>(n); // type:float, tilesize:32x32, the numeber of threads:512
 	for(size_t n=256;n<=32*1024;n=n*2)
-		execGPUkerenel<float,  64, 512>(n);
+		execGPUkernel<float,  64, 512>(n);
     // Change dynamic Shared memory size (CC7.0~)
 	for(size_t n=256;n<=32*1024;n=n*2)
-		execGPUkerenel<float, 128, 512>(n);
+		execGPUkernel<float, 128, 512>(n);
 
 	return 0;
 }
